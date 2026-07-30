781b8082d33be53579247b79821666688273c563
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2026-07-29 22:42:08 -0400

    made initial code for api server and first handler for create account

--- api/server.go
+++ api/server.go
@@ -1,1 +1,21 @@
 package api
+
+import (
+	db "github.com/MDelarosa1993/simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
