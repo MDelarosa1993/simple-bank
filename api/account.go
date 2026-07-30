62cbbce5a70c0478884d33916061eb847db7ee3d
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2026-07-29 22:47:42 -0400

    finish up createAccount handler

--- api/account.go
+++ api/account.go
@@ -3,11 +3,12 @@ package api
 import (
 	"net/http"
 
+	db "github.com/MDelarosa1993/simple-bank/db/sqlc"
 	"github.com/gin-gonic/gin"
 )
 
 type createAccountRequest struct {
-	Owner string `json:"owner" binding:"required"`
+	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required, oneof=USD EUR"`
 }
 
@@ -16,5 +17,19 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
 	}
+
+	params := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, params)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	ctx.JSON(http.StatusOK, account)
 }
